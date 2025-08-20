231dfb38e736094bcccce845f66f549e3bc8bc5b
Tom Vo <tomvothecoder@gmail.com>
2025-08-20 12:16:13 -0700

    Update eslint rules and add convenience commands

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,17 +1,17 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
   env: {
@@ -22,10 +22,29 @@ module.exports = {
     ecmaFeatures: {
       jsx: true,
     },
-    sourceType: "module",
+    sourceType: 'module',
   },
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "@typescript-eslint/no-unused-vars": "warn",
+    // Best practices and standard rules
+    eqeqeq: 'error',
+    curly: 'error',
+    'no-eval': 'error',
+    'no-implied-eval': 'error',
+    'no-unused-vars': 'warn',
+    'no-console': 'warn',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'no-duplicate-imports': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
   },
+  // Ignore linting for shadcn UI components during prototyping to avoid
+  // unnecessary warnings/errors
+  ignorePatterns: ['src/components/ui/**', 'src/components/ui/use-toast.ts'],
 };
